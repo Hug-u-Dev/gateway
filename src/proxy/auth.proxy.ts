3c87587ebc5d7da143df54cea9b6f69a663d46a8
Ana Laura <alauraivani@gmail.com>
2023-10-27 18:42:31 -0300

    Update auth.proxy.ts

--- src/proxy/auth.proxy.ts
+++ src/proxy/auth.proxy.ts
@@ -1,7 +1,7 @@
 import { createProxyMiddleware, fixRequestBody } from "http-proxy-middleware";
 
 const proxyAuthOptions = {
-	target: "http://localhost:3004", // Auth URL
+	target: "https://auth-y71l.onrender.com", // Auth URL
 	changeOrigin: true, // Needed for virtual hosted sites
 	pathRewrite: {
 		"^/auth": "", // Remove the prefix when forwarding requests to the microservice
